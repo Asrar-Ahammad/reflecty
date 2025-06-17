69f628b90d5383575d8166ce2e485ff5bdee65ac
Asrar Ahammad <asrarahammadshaik@gmail.com>
2025-06-17 18:48:14 +0530

    Adjust Navbar width for improved responsiveness

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,7 +6,7 @@ function Navbar() {
     <>
       <nav className="relative z-50">
         <div className="w-full flex items-center justify-center">
-          <div className="flex items-center justify-between w-full md:w-[50vw] p-4 rounded-full bg-clip-padding backdrop-filter backdrop-blur-md bg-opacity-0 border-[1px] border-gray-100 fixed top-4 md:right-[25%]">
+          <div className="flex items-center justify-between w-[90%] md:w-[50vw] p-4 rounded-full bg-clip-padding backdrop-filter backdrop-blur-md bg-opacity-0 border-[1px] border-gray-100 fixed top-4 md:right-[25%]">
             <Link to="/" className="flex items-center justify-center gap-2">
               <Leaf className="size-6 md:size-8"/>
               <p className="font-semibold text-lg md:text-xl">Reflecty</p>
